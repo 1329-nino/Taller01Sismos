6148fa4bdcd7869e74f3858a202a2ae51fea7cb5
nino <n.diaz10@ufromail.cl>
2021-09-01 09:28:20 -0400

    Agregadas funciones buscarMayorSismo contarSismos enviarSMS

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
+        var arreglo = crearArreglo();
+
 
     }
 
@@ -15,5 +20,44 @@ public class Main {
             }
         }
         return arreglo;
+    }
+
+    public static double buscarMayorSismo(Double[][] arreglo) {
+
+        double auxiliar = arreglo[0][0];
+
+        for (int i = 0; i < arreglo.length; i++) {
+            for (int j = 0; j < arreglo[0].length; j++) {
+                if (arreglo[i][j] > auxiliar) {
+                    auxiliar = arreglo[i][j];
+                }
+            }
+        }
+        return auxiliar;
+    }
+
+    public static double contarSismos(Double[][] arreglo) {
+
+        double contador = 0;
+
+        for (int i = 0; i < arreglo.length; i++) {
+            for (int j = 0; j < arreglo[0].length; j++) {
+                if (arreglo[i][j] >= 5.0) {
+                    contador++;
+                }
+            }
+        }
+        return contador;
+    }
+
+    public static void enviarSMS(Double[][] arreglo) {
+
+        for (int i = 0; i < arreglo.length; i++) {
+            for (int j = 0; j < arreglo[0].length; j++) {
+                if (arreglo[i][j] >= 7.0) {
+                    System.err.println("Alerta!!! se debe evacuar zona costera!");
+                }
+            }
+        }
     }
 }
